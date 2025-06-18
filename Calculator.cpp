7c1dedb889e07380ebb1946a13621b2ed46b59ba
Aditya Narayan <aditya.narayan0511@gmail.com>
2025-06-19 01:53:24 +0530

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -7,17 +7,22 @@ int main()
     float num1, num2, Result;
     char opp;
 
+    cout << "=============================" << endl;
+    cout << "     C++ BASIC CALCULATOR    " << endl;
+    cout << "=============================" << endl;
+
     cout << "Enter the first number:" << endl;
     cin >> num1;
 
-    cout << "Enter the second nuber:" << endl;
+    cout << "Enter the second number:" << endl;
     cin >> num2;
 
-    cout << "Enter the operation you wish to perform(+,-,*,/):" << endl;
+    cout << "Enter the operation you wish to perform (+, -, *, /):" << endl;
     cin >> opp;
 
     cout << "You have entered: " << num1 << " " << opp << " " << num2 << endl;
 
+    // Using switch case to perform the operation based on user input
     switch (opp)
     {
     case '+':
@@ -32,7 +37,6 @@ int main()
         Result = num1 * num2;
         cout << "The Result is: " << Result << endl;
         break;
-
     case '/':
         if (num2 != 0)
         {
@@ -47,6 +51,11 @@ int main()
     default:
         cout << "Invalid operation entered. Please use +, -, *, or /." << endl;
         break;
-        return 0;
     }
+
+    cout << "-----------------------------" << endl;
+    cout << "Thanks for using the Calculator!" << endl;
+    cout << "Keep Calculating !!" << endl;
+
+    return 0;
 }
